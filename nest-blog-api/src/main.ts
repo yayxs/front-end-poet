df83d3cce1bd3c0e011405f0f0c72246be94a57e
yayxs <leaderywl@163.com>
2020-01-10 23:06:43 +0800

    feat: add OpenAPI (Swagger)

--- nest-blog-api/src/main.ts
+++ nest-blog-api/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJs 博客API')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
