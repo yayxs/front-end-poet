423545f040a7559985a6cbf3ad5c29ab0baeceb5
WuRong <1667629529@qq.com>
2020-01-10 23:06:43 +0800

    增加OpenAPI (Swagger)

--- nest-blog-api/src/posts/posts.controller.ts
+++ nest-blog-api/src/posts/posts.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('posts')
 export class PostsController {
   @Get()
   index() {
     return [{ id: 1 }, { id: 1 }, { id: 1 }, { id: 1 }];
+  }
+  @Post()
+  create() {
+    return {
+      success: true
+    };
+  }
+  @Get(':id')
+  getPostById() {
+    return {
+      id: 1,
+      con: 'hahahhh'
+    };
   }
 }
