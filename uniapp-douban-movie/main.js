cc8b3911f538bfbff5952763eb87af6b7a49d188
yayxs <leaderywl@163.com>
2020-02-04 10:29:56 +0800

    '网络请求封装'

--- uniapp-douban-movie/main.js
+++ uniapp-douban-movie/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import MinRequest from './common/api/minRequest.js'
+import minRequest from './common/api/api.js'
 import './common/iconfont.css' // 引入阿里图标库
 Vue.config.productionTip = false
-
+Vue.use(MinRequest)
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+    ...App,
+	minRequest
 })
 app.$mount()
