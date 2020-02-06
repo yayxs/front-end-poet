92236d17d143bdbdef9027daa4a619138f69bea7
yayxs <leaderywl@163.com>
2020-02-06 21:20:35 +0800

    '基本完成博客接口的增删改查'

--- nest-blog-api/src/app.module.ts
+++ nest-blog-api/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
-
+import { TypegooseModule } from "nestjs-typegoose";
 @Module({
-  imports: [PostsModule],
+  imports: [
+    TypegooseModule.forRoot("mongodb://127.0.0.1:27017/nest-blog-api", {
+      useNewUrlParser: true
+    }),
+    
+    PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
