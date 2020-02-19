0a5b7b407a2c21ab82ad8a09bb44e9542e557a50
yangwl <yangwl@zonekey.com.cn>
2020-02-19 16:41:27 +0800

    [node]上传node

---
+++ nodeDemos1/00-exports.js
@@ -0,0 +1,9 @@
+let username = `yaxys`;
+let password = 123456;
+
+console.log(module.exports === exports);
+
+exports.obj = {
+  username,
+  password
+};
---
+++ nodeDemos1/README.md
@@ -0,0 +1,8 @@
+全局安装`nodemon`
+
+### 模块 module
+
+- exports
+- module.exports
+
+`exports`在模块执行之前赋值给`module.exports` ，实际上导出的是`module.rxports`
