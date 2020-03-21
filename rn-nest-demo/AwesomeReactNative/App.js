e8cf2f6a2ee99506ec6b7705f869f0ba4835cf53
yayxs <leaderywl@163.com>
2020-03-21 13:12:03 +0800

    demo

--- rn-nest-demo/AwesomeReactNative/App.js
+++ rn-nest-demo/AwesomeReactNative/App.js
@@ -1,53 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  PickerIOSComponent,
-} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
+import {StyleSheet, View, Text} from 'react-native';
+const App = () => {
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View style={styles.hello}>
-          <Text>hello</Text>
-        </View>
-      </SafeAreaView>
+      <View style={styles.hello}>
+        <Text> hell212121o</Text>
+      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
   hello: {
-    margin: 10,
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-    color: 'red',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#f0f',
   },
 });
 
