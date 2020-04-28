cf7cc0db6712440bd9a24474d8a2031b1f2aa8ef
yangwl <yangwl@zonekey.com.cn>
2020-04-28 18:29:14 +0800

    [vue-next-learn]

---
+++ vue-next-learn/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="increment">
+    当前的状态是： {{ state.count }}, 当前的状态乘以2计算得{{ state.double }}
+  </button>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2),
+    });
+
+    function increment() {
+      state.count++;
+    }
+
+    return {
+      state,
+      increment,
+    };
+  },
+};
+</script>
