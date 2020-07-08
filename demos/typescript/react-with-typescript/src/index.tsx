d036636102bec033ba7b37fbf8e675e2c69bcc7c
yangwl <yangwl@zonekey.com.cn>
2020-07-08 17:43:12 +0800

    ts

--- demos/typescript/react-with-typescript/src/index.tsx
+++ demos/typescript/react-with-typescript/src/index.tsx
@@ -1,42 +1,36 @@
-import React, { FunctionComponent, useState, FC ,useEffect} from "react";
+import React, {
+  FunctionComponent,
+  useState,
+  FC,
+  useEffect,
+  useContext,
+  useRef,
+  useReducer,
+  Component,
+  MouseEvent,
+  ReactNode,
+  SFC,
+} from "react";
 import * as ReactDOM from "react-dom";
 
+type Props = {
+  onClick(e: MouseEvent<HTMLElement>): void;
+  color?: string;
+};
 
+const Button: SFC<Props> = ({ onClick: handleClick, color, children }) => (
+  <button style={{ color }} onClick={handleClick}>
+    {children}
+  </button>
+);
 
-const Simple:FC=()=>{
-  
-
-  const [name,setName] =  useState('yayxs')
-  const [width,setWidth] = useState(0)
-  useEffect(() => {
-    
-    return () => {
-      document.title = `Hello ${name}`;
-    }
-  }, [name])
-
-  // 事件的派发监听
-  useEffect(() => {
-    const eventHandler = ()=>{
-     setWidth(Number(window.innerWidth))
-    }
-    window.addEventListener('resize', eventHandler);
-    return () => {
-      window.removeEventListener('resize', eventHandler);
-    }
-  }, [name])
+const App = () => {
+  const handleClick = ()=>{
+    console.log(`点击了按钮`)
+  }
   return (
     <>
-    <h4>{width}</h4>
-    </>
-  )
-}
-
-
-const App: FC = () => {
-  return (
-    <>
-      <Simple />
+      <Button onClick={handleClick }>按钮</Button>
     </>
   );
 };
