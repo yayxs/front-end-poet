a69aca98ca95bc9e0162cd4c020ce612094a4add
yangwl <yangwl@zonekey.com.cn>
2020-07-09 18:17:05 +0800

    add ts

--- demos/typescript/react-with-typescript/src/index.tsx
+++ demos/typescript/react-with-typescript/src/index.tsx
@@ -10,29 +10,22 @@ import React, {
   MouseEvent,
   ReactNode,
   SFC,
+  Fragment, PureComponent
 } from "react";
 import * as ReactDOM from "react-dom";
 
-type Props = {
-  onClick(e: MouseEvent<HTMLElement>): void;
-  color?: string;
-};
 
-const Button: SFC<Props> = ({ onClick: handleClick, color, children }) => (
-  <button style={{ color }} onClick={handleClick}>
-    {children}
-  </button>
-);
-
-const App = () => {
-  const handleClick = ()=>{
-    console.log(`点击了按钮`)
-  }
+type Value = { value:string }
+const App = ()=>{
+    // 其中 ref 是 存值的作用
+  // const ref = useRef<Value>({ value: "" })
+  // ref 是html 元素
+  const ref = useRef<HTMLDivElement>(null);
   return (
     <>
-      <Button onClick={handleClick }>按钮</Button>
+      <div ref={ref} ></div>
     </>
-  );
-};
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById("example"));
