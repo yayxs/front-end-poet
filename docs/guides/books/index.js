ca415653545e5ccd87ab5e1355fecce74f9f4641
yayxs <leaderywl@163.com>
2020-11-04 20:36:56 +0800

    feat:添加js高程

--- docs/guides/books/index.js
+++ docs/guides/books/index.js
@@ -1,27 +1,12 @@
-let o = {
-  id: 1,
-  info: {
-    arr: [1, 2, 3],
-    subs: [
-      {
-        id: 1,
-        name: "subs",
-        info: undefined, // 直接pass
-      },
-    ],
-  },
-};
+let dom = document.getElementById("file_id");
+dom.addEventListener("change", (e) => {
+  console.log(e.target.files);
+  let files = e.target.files,
+    i = 0,
+    len = files.length;
 
-console.log(JSON.stringify(o, ["id"])); // {"id":1}
-console.log(
-  JSON.stringify(o, (k, v) => {
-    if (k === "id") {
-      return 2;
-    } else {
-      return v;
-    }
-  })
-);
-
-console.log(JSON.stringify(o, null, 2));
-console.log(JSON.stringify(o, null, "---"));
+  while (i < len) {
+    console.log(files[i]);
+    i++;
+  }
+});
--- docs/guides/books/js_advanced4_23.md
+++ docs/guides/books/js_advanced4_23.md
@@ -2,17 +2,23 @@
 title: JSON
 ---
 
-### keywords
+## keywords
 
 - JSON 不属于 js
 - 解析序列化 json 的能力
 - JSON 中没有变量声明
 - 最后没有分号
-- ES5 在增加了 JSON 的全局对象
+- ES5 中增加了 JSON 的全局对象
 
 ## JSON.stringify
 
-**函数属性** **Symbol 类型** **存储的是undefined** 都是会被跳过，完整的形式 `JSON.stringify(val[,replacer,space])`
+### 是什么
+
+`JSON.stringify` 是将对象转换为 JSON，完整的形式 `JSON.stringify(val[,replacer,space])` 
+
+- 参数一 待转换的值
+- 参数二 可以是个数组、过滤函数
+- 参数三 格式化的缩进
 
 ```js
 let o = {
@@ -45,3 +51,8 @@ console.log(JSON.stringify(o, null, "---"));
 ```
 
 ## JSON.parse
+
+完整的形式`JSON.parse(str, [reviver]);`
+
+- 参数一 合法的字符串
+- 参数二 转换的函数
