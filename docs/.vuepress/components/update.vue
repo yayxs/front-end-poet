4990324b74ab71bf1b9666eaf8f8411392bca6da
yayxs <leaderywl@163.com>
2020-11-16 23:17:50 +0800

    feat:添加旧版的链接

---
+++ docs/.vuepress/components/update.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>{{ date }}</div>
+</template>
+
+<script>
+export default {
+  name: "Update",
+  data() {
+    return {
+      date: "",
+    };
+  },
+  methods: {
+    init() {
+      let d = new Date();
+      this.date = d;
+    },
+  },
+  mounted() {
+    this.init();
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
