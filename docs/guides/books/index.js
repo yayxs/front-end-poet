d00f7607cc8da38ade464ea1e5a4047397fa0b6b
yangwl <yangwl@zonekey.com.cn>
2020-12-08 18:31:01 +0800

    feat:添加http

--- docs/guides/books/index.js
+++
@@ -1,8 +0,0 @@
-function Person(name, age, job) {
-  this.name = name;
-  this.age = age;
-  this.job = job;
-}
-
-let hanshudeprototypeshuxing = Person.prototype;
-console.log(hanshudeprototypeshuxing.constructor);
