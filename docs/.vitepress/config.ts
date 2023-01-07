6483477aa7f47b868ad05e370521699458a8c78a
yayxs <leaderywl@163.com>
2023-01-07 22:53:22 +0800

    update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -20,8 +20,6 @@ export default defineConfig({
     sidebar: {
       '/html/': sidebarHTML(),
       '/vite/': sidebarVite(),
-      //   '/guide/': sidebarGuide(),
-      //   '/config/': sidebarConfig(),
     },
 
     editLink: {
