eb931b00e6fe1def05947f621411d6388363b685
yayxs <leaderywl@163.com>
2024-08-16 15:27:55 +0800

    chore: upgrade deps

---
+++ pages/[...slug].vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <!-- ContentDoc returns content for `$route.path` by default or you can pass a `path` prop -->
+    <ContentDoc />
+  </main>
+</template>
---
+++ src/entry-client.tsx
@@ -0,0 +1,11 @@
+console.log('entry-client.tsx run=======')
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
